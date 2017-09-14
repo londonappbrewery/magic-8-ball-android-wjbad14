401f35b20a59d63414fcc61f20ab7de983d6ee32
wjbad14 <30443852+wjbad14@users.noreply.github.com>
2017-09-14 22:33:00 +0200

    Challenge  Number 2 Completed

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,15 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton;
+        askButton = (Button) findViewById(R.id.askButton);
+
+        final ImageView ball = (ImageView) findViewById(R.id.image_ball);
+
+        final int[] ballArray = {R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+
+        };
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Magic-8-ball", "Ze Pressed the Button!");
+
+                Random randomImageGenerator = new Random();
+
+                int image = randomImageGenerator.nextInt(5);
+
+                Log.d("Magic-8-ball", "The next image is: " + image);
+
+                ball.setImageResource(ballArray[image]);
+            }
+
+        });
     }
 }
